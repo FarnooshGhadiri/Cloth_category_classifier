51b56455c80778300e6e7e1e48a64f4c5cbca91a
Farnoosh Ghadiri <ghadiri.farnoosh@gmail.com>
2019-02-06 19:16:08 -0500

    Update util.py

--- util/util.py
+++ util/util.py
@@ -163,10 +163,10 @@ def opt2file(opt, dst_file):
     args = vars(opt) 
     with open(dst_file, 'wt') as opt_file:
         opt_file.write('------------ Options -------------\n')
-        print '------------ Options -------------'
+        print ('------------ Options -------------')
         for k, v in sorted(args.items()):
             opt_file.write('%s: %s\n' % (str(k), str(v)))
-            print "%s: %s" %(str(k), str(v))
+            print ("%s: %s" %(str(k), str(v)))
         opt_file.write('-------------- End ----------------\n')
-        print '-------------- End ----------------'
+        print ('-------------- End ----------------')
 
