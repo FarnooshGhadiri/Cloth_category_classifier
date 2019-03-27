a99b8f7ab0fee072aa0191c8080a24569558bd64
Farnoosh Ghadiri <ghadiri.farnoosh@gmail.com>
2019-03-27 17:18:58 -0400

    Update Cloth-attribute-classification.py

--- Cloth-attribute-classification.py
+++ Cloth-attribute-classification.py
@@ -131,7 +131,13 @@ def calc_accuracy(output_binary, output_softmax, target_softmax,target_binary, s
     Sum_all_corr_attr = torch.zeros(len(top_k), target_binary.size(1), dtype=torch.float)
     Sum_all_gt_attr = torch.zeros(len(top_k), target_binary.size(1), dtype=torch.float)
     S_target_binary=target_binary
-    # pred = (output_binary.cpu()>=0.50)
+    # It is not a fair accuracy computation if total number of actual labels (GT)in the ground truth is bigger than k. To compensate this, we make a new dynamic ground truth for each sample
+    # Make a new ground truth for each sample : keep those actual attributes that are matched to the predicted attributes (Correct_pred_bin)by CAC (Cloth Attribute classification), 
+    # then select P (P= K-number of correctly annotated attributes)attributes randomly from (1-{GT-Correct_pred_bin}) list.
+    # Ex: GT={1,1,1,0,1,1 and zero for all other (1000-6) attributes}
+    # Pre_attr={1,0,1 and zero for all other (1000-6) attributes }
+    # New-GT={1,0,1,0,1,0 and zero for all other (1000-6) attributes }
+    #sample from the remaining
     for k in top_k:
         target_binary=S_target_binary
         _, pred2 = output_binary.data.cpu().topk(k, 1, True, True)
